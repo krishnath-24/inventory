23fc3fc05b36bb6bb0e1f3b961b55e6c7ff9251c
Anuj Sharma <krishnath@Anujs-MacBook-Air.local>
2024-12-20 21:31:46 +0530

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,16 @@ import {store} from './store/store';
 import 'primeicons/primeicons.css';
 import { PrimeReactProvider } from 'primereact/api';
 import "primereact/resources/themes/lara-light-indigo/theme.css";
-import React from 'react';
-
-
 
 
 function App() {
 
   return (
-    <React.StrictMode>
        <PrimeReactProvider>
         <Provider store={store}>
           <Dashboard />
         </Provider>
       </PrimeReactProvider>
-    </React.StrictMode>
   )
 }
 
